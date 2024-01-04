f8c9740c905c3fffce252aecf9c13e33954df20a
Kacper Fus <fuskacper@student.agh.edu.pl>
2024-01-04 13:35:43 +0100

    ostateczna wersja

---
+++ code.py
@@ -0,0 +1,21 @@
+x=3
+i=2
+x = i+1
+def add(x,y):
+    x = x+y
+    return x
+while i<10:
+    add(x,1)
+for k in range(40,50):
+    def func_1(x):
+        return x
+    func_1(k)
+array=[1,2,3]
+array[2]=5
+if x<5:
+    x=6
+else:
+    x=4
+while x<1000:
+    for i in range(1,10):
+        add(x,5)
